8baf649708708bd496f1b5fd179f864e78f955c5
rienour <R.D.Eichenour@gmail.com>
2025-05-05 20:44:53 -0500

    [CSES] - Weird Algorithm initial submission
    This is the initial submission for the Weird Algorithm solution
    utilizing the intially planned pseudo code.
    
    - Add weirdalgorithm.cpp
    
    Signed-off-by: rienour <R.D.Eichenour@gmail.com>

---
+++ CSES/Introductory_Problems/Weird_Algorithm/weirdalgorithm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int weirdInput = 0;
+
+  cin >> weirdInput;
+
+  while(weirdInput != 1) {
+    // Print the current weird input as part of the out string
+    cout << weirdInput << " ";
+
+    // Check if weird input is even to determine operation
+    if(weirdInput % 2 == 0) {
+      weirdInput >>= 1;
+    } else {
+      weirdInput *= 3;
+      weirdInput ++;
+    }
+  }
+  cout << weirdInput << endl;
+
+  return 0;
+}
